a17e95dd70ae48b4861b148e4d87d5c03f01cbaf
falvik <spavin4@gmail.com>
2020-03-03 17:53:03 +0300

    Add files via upload
    Дополнил параметры инициализации

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,14 @@ importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: '493570643651'
+    apiKey: "AIzaSyBh8Xq1A02Hjh10rDgoUK3YfbKOyLsdJVg",
+    authDomain: "test-51e78.firebaseapp.com",
+    databaseURL: "https://test-51e78.firebaseio.com",
+    projectId: "test-51e78",
+    storageBucket: "test-51e78.appspot.com",
+    messagingSenderId: "493570643651",
+    appId: "1:493570643651:web:cd564f697b8d081b17b476",
+    measurementId: "G-LLFF9VTSRL"
 });
 
 const messaging = firebase.messaging();
