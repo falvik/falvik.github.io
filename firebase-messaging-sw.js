acdcbb20ccf01eda182e001f58ae31a188170326
falvik <spavin4@gmail.com>
2020-03-05 16:36:34 +0300

    Add files via upload
    Вариант без использования firebase в serviceWorker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,5 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
-
-firebase.initializeApp({
-    apiKey: "AIzaSyBh8Xq1A02Hjh10rDgoUK3YfbKOyLsdJVg",
-    authDomain: "test-51e78.firebaseapp.com",
-    databaseURL: "https://test-51e78.firebaseio.com",
-    projectId: "test-51e78",
-    storageBucket: "test-51e78.appspot.com",
-    messagingSenderId: "493570643651",
-    appId: "1:493570643651:web:cd564f697b8d081b17b476",
-    measurementId: "G-LLFF9VTSRL"
-});
-
-const messaging = firebase.messaging();
-
-messaging.setBackgroundMessageHandler(function(payload) {
-    const data = payload.data;
+self.addEventListener('push', (push) => {
+    const data = push.data.json().data;
     const notificationOptions = {body: data.body, tag: data.messageId};
     const notify = new Notification(data.title,notificationOptions);
     if(data.link) {
@@ -27,7 +10,7 @@ messaging.setBackgroundMessageHandler(function(payload) {
     }
     notify.onshow = () => setStatus(data.messageId, "SHOW");
     notify.onerror = (event) => setStatus(data.messageId, "ERROR", JSON.stringify(event));
-});
+})
 
 function setStatus(id, status, error) {
     console.log('Отправка статуса ' + status + " ");
@@ -44,7 +27,3 @@ function setStatus(id, status, error) {
         }
     })
 }
-
-self.addEventListener('push', function(e) {
-    var i = 0;
-})
