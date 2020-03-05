dc4523c4bda0d205d9e86be9d665a670655fac53
falvik <spavin4@gmail.com>
2020-03-05 16:53:52 +0300

    Add files via upload
    Вернул вариант с Firebase

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,22 @@
-self.addEventListener('push', (push) => {
-    const data = push.data.json().data;
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBh8Xq1A02Hjh10rDgoUK3YfbKOyLsdJVg",
+    authDomain: "test-51e78.firebaseapp.com",
+    databaseURL: "https://test-51e78.firebaseio.com",
+    projectId: "test-51e78",
+    storageBucket: "test-51e78.appspot.com",
+    messagingSenderId: "493570643651",
+    appId: "1:493570643651:web:cd564f697b8d081b17b476",
+    measurementId: "G-LLFF9VTSRL"
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    const data = payload.data;
     const notificationOptions = {body: data.body, tag: data.messageId};
     const notify = new Notification(data.title,notificationOptions);
     if(data.link) {
@@ -10,7 +27,7 @@ self.addEventListener('push', (push) => {
     }
     notify.onshow = () => setStatus(data.messageId, "SHOW");
     notify.onerror = (event) => setStatus(data.messageId, "ERROR", JSON.stringify(event));
-})
+});
 
 function setStatus(id, status, error) {
     console.log('Отправка статуса ' + status + " ");
