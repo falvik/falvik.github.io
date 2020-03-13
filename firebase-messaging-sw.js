07eb9e6751813792d06f04aa3ce0a8b1755e3340
falvik <spavin4@gmail.com>
2020-03-13 12:49:21 +0300

    Add files via upload
    Fetch

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,4 @@
 // firebase-messaging-sw.js
-importScripts('https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js');
 importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
 
@@ -26,33 +25,22 @@ messaging.setBackgroundMessageHandler(function (payload) {
         .catch((event) => setStatus(data.messageId, "ERROR", JSON.stringify(event)))
 });
 
-self.addEventListener('notificationclick', function(event) {
+self.addEventListener('notificationclick', function (event) {
     var link = event.notification.data.link
-    if(link) {
+    if (link) {
         clients.openWindow(link)
     }
 })
 
 function setStatus(id, status, error) {
     console.log('Отправка статуса ' + status + " ");
-    axios.post(setStatusUrl, {messageCode:id, state: status, errorMessage: error})
-        .then(()=>{
-            console.log('Статус отправлен');
-        })
+    fetch(setStatusUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({messageCode: id, state: status, errorMessage: error})
+    }).then(() => {
+        console.log('Статус отправлен');
+    })
 }
-
-// function setStatus(id, status, error) {
-//     console.log('Отправка статуса ' + status + " ");
-//     $.ajax(setStatusUrl, {
-//         type: 'POST',
-//         contentType: 'application/json',
-//         dataType: 'text',
-//         data: {messageCode:id, state: status, errorMessage: error},
-//         success: function () {
-//             console.log('Статус успешно отправлен')
-//         },
-//         error: function (jqXhr, textStatus, errorMessage) {
-//             console.error('Статус не отправлен: ' + errorMessage);
-//         }
-//     })
-//}
